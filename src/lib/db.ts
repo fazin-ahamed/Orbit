055ef4dfd771efed15f388320ca9b794c63b3d2e
Fazin Ahamed Pallikara Valappil <68944272+fazin-ahamed@users.noreply.github.com>
2025-10-30 14:28:49 +0400

    Backend Knex Fix

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,4 +1,21 @@
 import knex from 'knex';
-const config = require('../../knexfile.js');
 
-export const db = knex(config[process.env.NODE_ENV || 'development']);
+const config = {
+  client: 'pg',
+  connection: process.env.DATABASE_URL,
+  migrations: {
+    directory: './src/migrations',
+    tableName: 'knex_migrations',
+  },
+  seeds: {
+    directory: './src/seeds',
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  timezone: 'UTC',
+  debug: false,
+};
+
+export const db = knex(config);
